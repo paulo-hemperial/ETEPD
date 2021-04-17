7ddc03be04495af3b793292c4c989527c6d0aedd
Paulo <73966838+paulo-hemperial@users.noreply.github.com>
2021-04-17 14:35:15 -0300

    UPDATE

--- LPC/2C/ATV_1_2_e_3_Exercicios_e_Desafios_em_Python/8.py
+++
@@ -1,4 +0,0 @@
-reais = float(input('Quantos reais você ganha por hora? R$'))
-total = reais*730.001
-##Essa é a quantidade de horas por mês, tio Google disse isso.
-print('Em um mês, você ganha em cerca de {:.2f}R$'.format(total))
