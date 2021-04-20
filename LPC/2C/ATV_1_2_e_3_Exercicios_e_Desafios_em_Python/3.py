2f858bb8c4f5c8441ae078b99df3bdec835ff7d4
Paulo <73966838+paulo-hemperial@users.noreply.github.com>
2021-04-20 17:15:25 -0300

    Update 3.py

--- LPC/2C/ATV_1_2_e_3_Exercicios_e_Desafios_em_Python/3.py
+++ LPC/2C/ATV_1_2_e_3_Exercicios_e_Desafios_em_Python/3.py
@@ -1,4 +1,4 @@
 n1 = int(input('Digite um número: '))
-n2 = int(input('Digite otro número: '))
+n2 = int(input('Digite outro número: '))
 soma = n1 + n2
 print('A soma dos dois números são iguais a ', soma)
