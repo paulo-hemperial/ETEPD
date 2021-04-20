adff3b10de1b1520cfc9f257457e6fc9511ca98d
Paulo <73966838+paulo-hemperial@users.noreply.github.com>
2021-04-20 17:24:55 -0300

    Delete LPC/1C directory

--- LPC/1C/5.py
+++
@@ -1,3 +0,0 @@
-metros = float(input('Digite uma quantidade de metros qualquer: '))
-centimetros = metros*100
-print('{:.2f}m em centimetros, são {:.2f}cm.'.format(metros, centimetros))
--- LPC/1C/9.py
+++
@@ -1,6 +0,0 @@
-n = float(input('Digite um número: '))
-if n >= 0:
-        print('Seu número é positivo!')
-elif n <0:
-        print('Seu número é negativo.')
-##Por que voce escreveria letras?
