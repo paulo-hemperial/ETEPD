0ec4d518bc44490d1569a5db7289c8f6d982d4b2
Paulo <73966838+paulo-hemperial@users.noreply.github.com>
2021-04-20 17:16:05 -0300

    Update 5.py

--- LPC/2C/ATV_1_2_e_3_Exercicios_e_Desafios_em_Python/5.py
+++ LPC/2C/ATV_1_2_e_3_Exercicios_e_Desafios_em_Python/5.py
@@ -1,3 +1,3 @@
 metros = float(input('Digite uma quantidade de metros qualquer: '))
 centimetros = metros*100
-print('{:.2f}m em centimetros, são {:.2f}cm.'.format(metros, centimetros))
+print('{:.2f}m em centimetros, dão {:.2f}cm.'.format(metros, centimetros))
